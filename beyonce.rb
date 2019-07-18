789f70617844b65ea4221e325c239e376f8ec9d8
Maddy Austin <maddy.austin@yahoo.co.uk>
2019-07-18 13:44:57 +0000

    Automatically backed up by Learn

--- beyonce.rb
+++ beyonce.rb
@@ -1,2 +1,2 @@
 num_of_attendees = 547
-puts "There are " + num_of_attendees.string + " people coming to beyonce's birthday party."
+puts "There are " + "num_of_attendees.string" + " people coming to beyonce's birthday party."
